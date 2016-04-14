a66eb5702f7f65cf474efaa84cec8bba8327f6e1
Javi <Javi@192.168.1.180>
2016-04-14 18:33:51 +0200

    LogViewer añadido, falta retocar la concurrencia del get

--- Ejercicio6.0/src/es/upm/dit/adsw/ej60/TsList.java
+++ Ejercicio6.0/src/es/upm/dit/adsw/ej60/TsList.java
@@ -4,11 +4,12 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class TsList {
+	private LogViewer viewer=LogViewer.getInstance();
 	List<CV> lista = new ArrayList<CV>();
 	int id;
 
 	public synchronized void put(String clave, String valor) {
-
+		viewer.dump(this, 0, 1);
 		if ((clave == null) || (clave.isEmpty() == true)) {
 			throw new IllegalArgumentException();
 		}
@@ -20,12 +21,17 @@ public class TsList {
 			}
 		}
 		lista.add(new CV(clave, valor));
+		viewer.dump(this, 0, 0);
+
 	}
 
-	public synchronized String get(String clave) {
+	public String get(String clave) {
+		viewer.dump(this, 1, 0);
 
 		for (CV cv : lista) {
 			if (cv.getClave().compareTo(clave) == 0) {
+				viewer.dump(this, 0, 0);
+
 				return cv.getValor();
 			}
 		}
@@ -33,16 +39,19 @@ public class TsList {
 	}
 
 	public synchronized String remove(String clave) {
-
+		viewer.dump(this, 0, 1);
 		for (CV cv : lista) {
 			if (cv.getClave().equals(clave)) {
 				String valorBorrado = cv.getValor();
 				lista.remove(cv);
+				viewer.dump(this, 0, 0);
+
 				return valorBorrado;
 			}
 		}
 
 		return null;
+		
 	}
 
 	public synchronized void clear() {
